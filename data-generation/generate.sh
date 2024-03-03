a916d02f06c07bea8c752e351e2657ed3355f15e
Manuel Kienlein <manuel.kienlein@gmx.de>
2024-03-03 18:36:23 +0100

    Added gzip compression and S3 upload to tpc-h generate script

--- data-generation/generate.sh
+++ data-generation/generate.sh
@@ -3,10 +3,14 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd $parent_path/tpc-h/dbgen
 
+output_dir=$parent_path/tpc-h/dbgen/output
+mkdir $output_dir
+export DSS_PATH=$output_dir
+
 # == DBGEN ==
 # -s <n>: Scale factor in <s> GB
 # -T: schema table name
-# -f: Force, overwrite exisiting files
+# -f: Force, overwrite existing files
 # -b <s>: Load distribution for <s> (default: dists.dss)
 # -C <n>: Split data set into <n> chunks
 # -S <n>: Build the <n>th step of data set
@@ -14,7 +18,40 @@ cd $parent_path/tpc-h/dbgen
 #./dbgen -s 1
 #./dbgen -s 1 -C 5 -S 1
 
-for i in {1..5}
+SCALE_FACTOR=1
+PARTITIONS=5
+
+DBGEN_PATH=./dbgen
+
+S3_BUCKET=tpc-h-benchmark
+
+
+for (( i=0; i<=$PARTITIONS; i++ ))
 do
-   ./dbgen -s 1 -C 5 -S $i -f
+  echo $DBGEN_PATH -s $SCALE_FACTOR -C $PARTITIONS -S $i -f -v
+  $DBGEN_PATH -s $SCALE_FACTOR -C $PARTITIONS -S $i -f -v
+
+  output_files=$output_dir/*
+  for file in $output_files
+  do
+    # Extract file name and table name
+    file_name=$(basename -- "$file")
+    table=$(echo $file_name | cut -d'.' -f1)
+
+    # Skip already processed files
+    #case $file_name in *.gz.csv) continue;; esac
+
+    # Compress with gzip
+    #gzip -v $file
+    gzip -c $file > $file.csv.gz
+    rm $file
+    file = $file.csv.gz
+
+    # Copy files to S3
+    echo aws s3 cp $file s3://$S3_BUCKET/sf$SCALE_FACTOR/$table/$file_name.csv
+    aws s3 cp $file s3://$S3_BUCKET/sf$SCALE_FACTOR/$table/$file_name.csv
+
+    # Delete file after upload
+    rm $file
+  done
 done
